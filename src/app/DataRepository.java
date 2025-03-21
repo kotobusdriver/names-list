5257601b76d474066a5fc88eaf4535a9320f6b3a
kotobusdriver <utkins@gmail.com>
2025-03-21 17:36:47 +0200

    Initial commit

---
+++ src/app/DataRepository.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRepository {
+    // Метод повертає список імен
+    public List<String> getData() {
+        List<String> list = ArrayList<>();
+
+        return list;
+    }
+}
