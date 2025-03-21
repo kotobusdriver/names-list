e8194a001b72a4b35969ea4f5813e3ee174e886e
kotobusdriver <utkins@gmail.com>
2025-03-21 17:45:24 +0200

    input data and fix data handling

--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -6,8 +6,11 @@ import java.util.List;
 public class DataRepository {
     // Метод повертає список імен
     public List<String> getData() {
-        List<String> list = ArrayList<>();
-
+        String[] array = {"Alice", "Bob", "Lucy", "Denis", "Tom"};
+        List<String> list = new ArrayList<>();
+        for (String s : array) {
+            list.add(s);
+        }
         return list;
     }
 }
